6a64723ed7fb4725a03601cfd3f4b882612ced14
Moshe Zadka <zadka.moshe@gmail.com>
2015-09-30 21:42:00 -0700

    +crypto pseduo code

---
+++ boredbot/crypto.py
@@ -0,0 +1,7 @@
+
+def decryptDict(dct, signingPrivateKey, realPrivateKey, realPublicKey):
+    ## Check that realPublicKey and realPrivateKey match
+    pass
+
+def encrypt(value, signingPrivateKey, realPublicKey):
+    ## Encrypt from signingPrivateKey to realPublicKey
