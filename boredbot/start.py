412d24b4fe0e3eec0ac84c180b48a88860a3d3c1
Moshe Zadka <zadka.moshe@gmail.com>
2015-10-03 10:50:11 -0700

    move to gunicorn

--- boredbot/start.py
+++ boredbot/start.py
@@ -32,7 +32,9 @@ def main(args):
     for dr in places:
         os.mkdir(dr)
     cmdLine = calcCommandline()
-    ctllib.add(places, 'boredbot', cmd=cmdLine[0], args=cmdLine[1:] + ['loop'], env=['SECRET_KEY='+os.environ['SECRET_KEY']])
-    ctllib.add(places, 'boredweb', cmd=cmdLine[0], args=cmdLine[1:] + ['web'], env=['SECRET_KEY='+os.environ['SECRET_KEY']])
+    ctllib.add(places, 'boredbot', cmd=cmdLine[0], args=cmdLine[1:] + ['loop'],
+               env=['SECRET_KEY='+os.environ['SECRET_KEY']])
+    ctllib.add(places, 'boredweb', cmd=cmdLine[0], args=cmdLine[1:] + ['gunicorn', '-w', '4', 'boredbot.web:app'],
+               env=['SECRET_KEY='+os.environ['SECRET_KEY']])
     sys.argv = ['twistd', '--nodaemon', 'ncolony', '--messages', messages, '--config', config]
     twistd.run()
