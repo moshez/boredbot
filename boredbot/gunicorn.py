5c12332fe4dfcf75c8d409827b2df0a77061bcd5
Moshe Zadka <zadka.moshe@gmail.com>
2015-10-03 10:48:24 -0700

    just plain shim into gunicorn

--- boredbot/gunicorn.py
+++ boredbot/gunicorn.py
@@ -1,21 +1,9 @@
 from __future__ import absolute_import
+import sys
 
-from gunicorn import config
-from gunicorn.app import base as gabase
-
-from boredbot import web
+from gunicorn.app import wsgiapp
 
 BOREDBOT_MAIN_OK = True
-
-class NoLoadApplication(gabase.BaseApplication):
-    def load_config(self):
-        pass
-
 def main(args):
-    gapp = NoLoadApplication()
-    gapp.callable = web.app
-    # gapp.cfg.address = '0.0.0.0:5000'
-    workers = config.Setting()
-    workers.value = 4
-    gapp.cfg.settings['workers'] = workers
-    gapp.run()
+    sys.argv = args
+    wsgiapp.run()
