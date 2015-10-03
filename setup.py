76c72b1d4d8a4034f485154e1655d37c1400f91d
Moshe Zadka <zadka.moshe@gmail.com>
2015-10-02 20:20:29 -0700

    +setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# Copyright (c) Moshe Zadka
+# See LICENSE for details.
+from distutils import cmd, spawn
+
+import os
+import subprocess
+import sys
+
+import setuptools
+
+
+setuptools.setup(
+    url='https://github.com/moshez/boredbot',
+    name='boredbot',
+    packages=setuptools.find_packages(),
+    version='0.0.0',
+)
