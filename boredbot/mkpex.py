e62e0144a3d5c9b43ada0e943e6739bd48ef5a12
Moshe Zadka <zadka.moshe@gmail.com>
2015-10-02 19:04:12 -0700

    temp commit

--- boredbot/mkpex.py
+++ boredbot/mkpex.py
@@ -1,9 +1,22 @@
+import os
+import shutil
+import subprocess
+
 import pex
 
 def buildWheels():
+    shutil.rmtree('build/wheelhouse')
     subprocess.check_call(['pip', 'wheel', '--requirement', 'requirements.txt', '--wheel-dir', 'build/wheelhouse'])
+    subprocess.check_call(['pip', 'wheel', 'setuptools==15.2', '--wheel-dir', 'build/wheelhouse'])
     subprocess.check_call(['pip', 'wheel', '.', '--wheel-dir', 'build/wheelhouse'])
 
+def buildPEX():
+    if os.path.exists('build/boredbot.pex'):
+        os.remove('build/boredbot.pex')
+    subprocess.check_call(['pex', '--repo', 'build/wheelhouse', '--no-index', '--output-file', 'build/boredbot.pex',
+                           '--requirement', 'requirements.txt', 'boredbot', '--entry-point', 'boredbot'])
+
 BOREDBOT_MAIN_OK = True
-def main():
+def main(dummyArgs):
     buildWheels()
+    buildPEX()
