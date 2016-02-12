37c8bdebf2268d9bf3c69b3bc4654450756954eb
Moshe Zadka <zadka.moshe@gmail.com>
2016-02-12 09:53:07 -0800

    froop

--- boredbot/parse.py
+++
@@ -1,6 +0,0 @@
-def getHeaders(appID, restKey):
-    return {
-        "X-Parse-Application-Id": appID,
-        "X-Parse-REST-API-Key": restKey,
-        "Content-Type": "application/json",
-    }
--- boredbot_deploy/mkpex.py
+++
@@ -1,8 +0,0 @@
-## Build a Pex from the requirements and the local 'boredbot' wheel
-## Behave as though it's running 'python -m boredbot_deploy'
-from luggage import buildpex
-
-BOREDBOT_MAIN_OK = True
-def main(dummyArgs):
-    wheelhouse = buildpex.buildWheels('build', 'requirements.txt')
-    buildpex.buildPEX(wheelhouse, 'build/boredbot.pex', 'requirements.txt', 'boredbot_deploy', ['boredbot'])
